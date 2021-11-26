728fcb1575bfbc7ed79e8fe01aeca8d36213fb2a
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2021-11-26 23:42:16 +0100

    add new fuction in js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
-const questionHeaders = document.querySelectorAll('.question-header');
+$(document).ready(function () {
 
-questionHeaders.forEach(questionHeader => {
-    questionHeader.addEventListener('click', event => {
-        questionHeader.classList.toggle('show');
-        const questionText = questionHeader.nextElementSibling;
-        if (questionHeader.classList.contains('show')) {
-            questionText.style.display = 'block';
-        }
-        else {
-            questionText.style.display = 'none';
-        }
+    const questionHeaders = document.querySelectorAll('.question-header');
+
+    questionHeaders.forEach(questionHeader => {
+        questionHeader.addEventListener('click', event => {
+            questionHeader.classList.toggle('show');
+            const questionText = questionHeader.nextElementSibling;
+            if (questionHeader.classList.contains('show')) {
+                questionText.style.display = 'block';
+            }
+            else {
+                questionText.style.display = 'none';
+            }
+        });
     });
-});
 
 
 
-$('button').on('click', function () {
-    $('body, html').animate({
-        scrollTop: $('.contact').offset().top
-    }, 1500);
-});
+    $('button').on('click', function () {
+        $('body, html').animate({
+            scrollTop: $('.contact').offset().top
+        }, 1500);
+    });
+
+})
